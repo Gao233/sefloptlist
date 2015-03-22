6ec71c72e3a916a980bf5ffe49b93e5330496006
FangWang <TerryWang@FangWangdeMacBook-Pro.local>
2015-03-21 23:52:48 -0500

    Finishedgit add --all Yeah~~

--- MTF.java
+++ MTF.java
@@ -2,23 +2,45 @@ public class MTF {
 	public static node head;
 
 	public static void main(String[] args) {
-		head = null;
-		//create a single node linked list with node from 0 to 9 as our initial list 
-		head = new node(0, new node(1, new node(2, new node(3, new node(4,
-			new node(5, new node(6, new node(7, new node(8, new node(9,null))))))))));
-		
-		for (node x = head; x != null; x = x.next) {
-			System.out.print(x);
+		head = new node(0, null);
+		// create a single node linked list with node from 0 to 9 as our initial
+		// list
+		head.next = new node(0, new node(1, new node(2, new node(3, new node(4,
+				new node(5, new node(6, new node(7, new node(8, new node(9,
+						null))))))))));
+
+		System.out.println("Initial dataset:");
+		printNodes();
+		String s = "123456789999999888888";
+		for (int i = 0; i < s.length(); i++) {
+			int value = Character.getNumericValue(s.charAt(i));
+			System.out.println("Value to move to front: " + value);
+			MoveToFront(value);
+			printNodes();
 		}
 	}
 
-	/*private void MoveToFront(int data) {
+	private static void MoveToFront(int input) {
 		node x = head;
-		while (x.data != data) {
+		while (x.next.data != input) {
 			x = x.next;
 		}
-		x.next = head;
-		head = 
+		if (x.next.next == null) {
+			x.next.next = head.next;
+			head.next = x.next;
+			x.next = null;
+		} else {
+			node temp = x.next;
+			x.next = temp.next;
+			temp.next = head.next;
+			head.next = temp;
+		}
+	}
 
-	}*/
+	private static void printNodes() {
+		for (node x = head.next; x != null; x = x.next) {
+			System.out.print(x);
+		}
+		System.out.println();
+	}
 }
